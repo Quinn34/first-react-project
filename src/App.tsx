e888460fb7383b562b33bb7ab0b28225d5b77d0d
Quinn34 <1207398@student.roc-nijmegen.nl>
2025-04-10 12:34:41 +0200

    links/Home

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import './App.css'
 import Header from'./Header.tsx'
 import Footer from './Footer.tsx'
 import Api from './Api.tsx'
+import Home from './Home.tsx'
 import Content from './content.tsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Favoriet from './Favoriet.tsx';
+import About from './About.tsx';
+import Kopen from './Kopen.tsx';
 import MarketShareChart from './Cirlcle.tsx'
 // ^benodigde imports
 
@@ -22,7 +25,10 @@ function App() {
         <Footer/>
         <BrowserRouter>
           <Routes>
+              <Route path="Home" element={<Home/>}/>
               <Route path="Favoriet" element={<Favoriet />}/>
+              <Route path="About" element={<About />}/>
+              <Route path="Kopen" element={<Kopen />}/>
           </Routes>
           {/* path naar alle andere paginas */}
         </BrowserRouter>
